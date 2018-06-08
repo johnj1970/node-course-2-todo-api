4c9b2def2d447ca7613ce53a78a12015d21f6f3c
jwalshamey <jonathan.walsh@amey.co.uk>
2018-06-08 15:13:34 +0100

    set up for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongodb://<dbuser>:<dbpassword>@ds147420.mlab.com:47420/johnj1970_todos
 module.exports = {mongoose};
